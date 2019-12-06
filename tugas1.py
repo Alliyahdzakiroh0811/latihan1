ecdf7e91098ab39d54d7d4925ccb1620615c6cfb
Alliyahdzakiroh0811 <56861575+Alliyahdzakiroh0811@users.noreply.github.com>
2019-12-06 20:58:20 +0700

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,9 @@
+namalengkap = "Alliyah Dzakiroh"
+namapanggilan = "Alyaa"
+npm = "311910551"
+tempatlahir = "bekasi"
+umur = "17"
+telf = "089526383527"
+alamat = "cibarusah"
+
+print("assalamualaikum, \n let me introduce my self, My name is",namalengkap,", but you can call me",namapanggilan,", my npm is",npm,", iwas born in",tempatlahir,"and i am",umur,"years old. i am very glad if you want to invite my house in",alamat,", so don't forget to call me before with the number",telf,"\n thank you")
